73c1266b07251c6210f5e089bbf91f7fde18a472
Aryano Devil <81142233+aryanotriks@users.noreply.github.com>
2021-04-21 05:25:07 +0000

    Create Badboy.py

--- Badboy.py
+++ Badboy.py
@@ -41,7 +41,7 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
 def keluar():
-	print 'Subscribe To Aryano Tricks'
+	print ('Subscribe To Aryano Tricks')
 	os.sys.exit()
 
 def acak(b):
@@ -97,7 +97,7 @@ logo = """
 \033[1;97m                /(                                 
 \033[1;94m               (__)                  
 
-\033[1;93m\033[1;92m\033[1;93m WhatsApp Number \033[1;94m\033[1;95m\033[1;93m  \033[1;96m\033[1;93m +923482860857 \033[1;92m\033[1;95m
+\033[1;93m\033[1;92m\033[1;93m WhatsApp Number \033[1;94m\033[1;95m\033[1;93m  \033[1;96m\033[1;93m +923472860857 \033[1;92m\033[1;95m
 \033[1;97m✮❂❂❂❂❂❂❂❂❂❂❂❂✮\033[1;91mMR-ROBOT\033[1;97m✮❂❂❂❂❂❂❂❂❂❂❂❂✮"""
 logo2 = """
 \033[1;96m▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇▇▇\033[1;91m▇▇▇▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇
